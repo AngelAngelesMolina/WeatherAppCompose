913f36a70df8cf74e2a00a1c4b8b0b86482eaf75
Angel Angeles Molina <82426196+AngelAngelesMolina@users.noreply.github.com>
2025-11-11 20:51:42 -0600

    ADD: WeatherState

---
+++ app/src/main/java/com/jaamcoding/myapplication/presentation/WeatherState.kt
@@ -0,0 +1,9 @@
+package com.jaamcoding.myapplication.presentation
+
+import com.jaamcoding.myapplication.domain.weather.WeatherInfo
+
+data class WeatherState(
+    val weatherInfo: WeatherInfo? = null,
+    val isLoading: Boolean = false,
+    val error: String? = null
+)
