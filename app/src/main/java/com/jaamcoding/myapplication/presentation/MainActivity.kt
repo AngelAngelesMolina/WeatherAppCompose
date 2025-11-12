21ecec4fce6c7b79ee3f8ad6265bc0f6235cc337
Angel Angeles Molina <82426196+AngelAngelesMolina@users.noreply.github.com>
2025-11-11 21:02:32 -0600

    UPDATE: MainActivty

--- app/src/main/java/com/jaamcoding/myapplication/presentation/MainActivity.kt
+++ app/src/main/java/com/jaamcoding/myapplication/presentation/MainActivity.kt
@@ -1,29 +1,56 @@
 package com.jaamcoding.myapplication.presentation
 
 import android.os.Bundle
+import android.Manifest
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.viewModels
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.jaamcoding.myapplication.presentation.components.WeatherCard
+import com.jaamcoding.myapplication.presentation.ui.theme.DarkBlue
+import com.jaamcoding.myapplication.presentation.ui.theme.DeepBlue
 import com.jaamcoding.myapplication.presentation.ui.theme.MyApplicationTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    private val vm : WeatherViewModel by viewModels()
+    private lateinit var permissionLauncher: ActivityResultLauncher<Array<String>>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        permissionLauncher = registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ) {
+            vm.loadWeatherInfo()
+        }
+        permissionLauncher.launch(arrayOf(
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+        ))
         setContent {
             MyApplicationTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(modifier = Modifier.fillMaxSize().background(DarkBlue)) { innerPadding ->
+                    Column(modifier = Modifier.fillMaxWidth().padding(innerPadding)) {
+                        WeatherCard(state = vm.state, backgroundColor = DeepBlue)
+                    }
+//                    Greeting(
+//                        name = "Android",
+//                        modifier = Modifier.padding(innerPadding)
+//                    )
                 }
             }
         }
