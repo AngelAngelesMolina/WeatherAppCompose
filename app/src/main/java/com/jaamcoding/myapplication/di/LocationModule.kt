b9baa4f6cbf708858618b6bb9e3a8c63303d962f
Angel Angeles Molina <82426196+AngelAngelesMolina@users.noreply.github.com>
2025-11-11 20:29:42 -0600

    ADD: LocationModule

---
+++ app/src/main/java/com/jaamcoding/myapplication/di/LocationModule.kt
@@ -0,0 +1,20 @@
+package com.jaamcoding.myapplication.di
+
+import com.jaamcoding.myapplication.data.location.DefaultLocationTracker
+import com.jaamcoding.myapplication.domain.location.LocationTracker
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import javax.inject.Singleton
+
+@ExperimentalCoroutinesApi
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class LocationModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindLocationTracker(defaultLocationTracker: DefaultLocationTracker): LocationTracker
+}
