01ea1d6c03036283ff0855af1d7c998ca70df243
Angel Angeles Molina <82426196+AngelAngelesMolina@users.noreply.github.com>
2025-11-11 20:28:58 -0600

    ADD: hilt root module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,17 +30,21 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "17"
     }
     buildFeatures {
         compose = true
     }
 }
-
+configurations.all {
+    resolutionStrategy {
+        force("com.squareup:javapoet:1.13.0")
+    }
+}
 dependencies {
 
     implementation(libs.androidx.core.ktx)
