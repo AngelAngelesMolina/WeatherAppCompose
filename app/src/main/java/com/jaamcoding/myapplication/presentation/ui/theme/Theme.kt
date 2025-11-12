3e124bbffb13584e3bbc09af74182605a7c95378
Angel Angeles Molina <82426196+AngelAngelesMolina@users.noreply.github.com>
2025-11-11 18:55:59 -0600

    ADD: Retrofit libraries

--- app/src/main/java/com/jaamcoding/myapplication/presentation/ui/theme/Theme.kt
+++ app/src/main/java/com/jaamcoding/myapplication/presentation/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
-package com.jaamcoding.myapplication.ui.theme
+package com.jaamcoding.myapplication.presentation.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
