2dafa2e6c0b276fede2e9b301d756f833887deb9
Angel Angeles Molina <82426196+AngelAngelesMolina@users.noreply.github.com>
2025-11-14 17:43:07 -0600

    ADD: WeatherScreen into mainActivity

--- app/src/main/java/com/jaamcoding/myapplication/presentation/MainActivity.kt
+++ app/src/main/java/com/jaamcoding/myapplication/presentation/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.jaamcoding.myapplication.presentation
 
-import android.os.Bundle
 import android.Manifest
+import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -9,25 +9,22 @@ import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
+import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.jaamcoding.myapplication.presentation.components.WeatherCard
+import androidx.compose.ui.graphics.Color
+import com.jaamcoding.myapplication.presentation.screens.WeatherScreen
 import com.jaamcoding.myapplication.presentation.ui.theme.DarkBlue
-import com.jaamcoding.myapplication.presentation.ui.theme.DeepBlue
 import com.jaamcoding.myapplication.presentation.ui.theme.MyApplicationTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-    private val vm : WeatherViewModel by viewModels()
+    private val vm: WeatherViewModel by viewModels()
     private lateinit var permissionLauncher: ActivityResultLauncher<Array<String>>
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,42 +35,28 @@ class MainActivity : ComponentActivity() {
         ) {
             vm.loadWeatherInfo()
         }
-        permissionLauncher.launch(arrayOf(
-            Manifest.permission.ACCESS_FINE_LOCATION,
-            Manifest.permission.ACCESS_COARSE_LOCATION,
-        ))
+        permissionLauncher.launch(
+            arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+            )
+        )
         setContent {
             MyApplicationTheme {
-                Scaffold(modifier = Modifier.fillMaxSize().background(DarkBlue)) { innerPadding ->
-                    Column(modifier = Modifier.fillMaxWidth().padding(innerPadding)) {
-                        WeatherCard(state = vm.state, backgroundColor = DeepBlue)
-                        Button(onClick = { vm.loadWeatherInfo() }) {
-                            Text(text = "Refresh")
-                        }
-                    }
-                    /*bf2ee22b237a7ae7ee0e594b079e228f*/
-//                    Greeting(
-//                        name = "Android",
-//                        modifier = Modifier.padding(innerPadding)
-//                    )
+                Scaffold(
+                    modifier = Modifier
+                        .fillMaxSize(),
+                    contentWindowInsets = WindowInsets(0), // 👈 evita que el scaffold agregue insets
+                    containerColor = Color.Transparent,
+                ) { inner ->
+                    WeatherScreen(
+                        Modifier
+                            .fillMaxSize()
+                            .padding(inner)
+                            .windowInsetsPadding(WindowInsets.safeDrawing) // respeta notch, status, nav
+                    )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyApplicationTheme {
-        Greeting("Android")
     }
 }
