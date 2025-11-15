7ef6df2b96e9b3a8bdf49ea3497b08158416379a
Angel Angeles Molina <82426196+AngelAngelesMolina@users.noreply.github.com>
2025-11-14 19:25:06 -0600

    CHG: LaunchEffect loadWeather
    Putting a launchEffect to send the fetch always

--- app/src/main/java/com/jaamcoding/myapplication/presentation/screens/WeatherScreen.kt
+++ app/src/main/java/com/jaamcoding/myapplication/presentation/screens/WeatherScreen.kt
@@ -7,10 +7,12 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -26,20 +28,30 @@ import com.jaamcoding.myapplication.presentation.ui.theme.DarkBlue
 import com.jaamcoding.myapplication.presentation.ui.theme.MyApplicationTheme
 
 @Composable
-fun WeatherScreen(modifier: Modifier, onRefreshClick : () -> Unit ={}) {
+fun WeatherScreen(modifier: Modifier, onRefreshClick: () -> Unit = {}) {
     val vm: WeatherViewModel = hiltViewModel()
     val state = vm.state  // o collectAsState()
-    Box(modifier) {
-        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-            HeaderTitle(city = state.weatherInfo?.name, date = "Tuesday 27", "08:41 AM")
-            Icon(painter = painterResource(state.weatherInfo?.weatherType?.iconRes ?: R.drawable.ic_sunny), contentDescription = "Icon row",  tint = Color.Unspecified)
-            TemperatureIndicator(modifier = Modifier.padding(24.dp), temperature = state.weatherInfo?.temperatureCelsius.toString(), tempDescription = state.weatherInfo?.weatherType?.weatherDesc ?: "")
-            Button(onClick = {vm.loadWeatherInfo()}) {
-                Text(text = "Refresh")
-            }
-        }
+    LaunchedEffect(true) {
+        vm.loadWeatherInfo()
     }
-
+    Box(modifier) {
+            Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+                HeaderTitle(city = state.weatherInfo?.name, date = "Tuesday 27", "08:41 AM")
+                Icon(
+                    painter = painterResource(
+                        state.weatherInfo?.weatherType?.iconRes ?: R.drawable.ic_sunny
+                    ), contentDescription = "Icon row", tint = Color.Unspecified
+                )
+                TemperatureIndicator(
+                    modifier = Modifier.padding(24.dp),
+                    temperature = state.weatherInfo?.temperatureCelsius.toString(),
+                    tempDescription = state.weatherInfo?.weatherType?.weatherDesc ?: ""
+                )
+                /* Button(onClick = {vm.loadWeatherInfo()}) {
+                     Text(text = "Refresh")
+                 }*/
+            }
+    }
 }
 
 @Preview(showBackground = true)
